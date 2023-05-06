e0b3ea100eea224d64adf1126bfa639e322aa3ac
bouquet1 <bukuleta@gmail.com>
2023-05-06 10:40:43 -0700

    add container to hold practiceList ul

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-console.log("test");
-fetch("http://localhost:3000/dailyQuickies").then(res => res.json()).then(json => console.log(json))
+//console.log("test");
+fetch("http://localhost:3000/dailyQuickies")
+    .then(res => res.json())
+    //.then(json => console.log(json))
+    .then(practices => {
+        //have the container for the practices
+        const practiceList = document.querySelector("ul#practicesList");
+        console.log(practiceList);
+    })
+
+
