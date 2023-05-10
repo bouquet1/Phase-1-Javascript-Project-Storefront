59d3fa1ebc5d0406a8aa848982bef419f7a7622a
bouquet1 <bukuleta@gmail.com>
2023-05-10 14:47:07 -0700

    cleans amd organizes code after lab session

--- main.js
+++ main.js
@@ -4,25 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
         //make a <div class="dailyQuickies"> and give a class name "daily-quickies-contanier"
         let dailyQuickies = document.createElement("div");
         dailyQuickies.classList.add("daily-quickies-contanier");
-        //create an element to hold the title and pull the title from json data 
+        //create an element to hold the title h2, img, duration, description, button and pull the title from json data 
         let h2 = document.createElement("h2");
         h2.textContent = practice.title;
-        //create an element to hold img 
         let img = document.createElement("img");
         img.src = practice.image;
-        img.classList.add("daily-quickies-images");
-        //create an element to hold duration    
+        img.classList.add("daily-quickies-images");  
         let h5 = document.createElement("h5");
         h5.textContent = practice.duration;
-        //create an element to hold description
         let p = document.createElement("p");
         p.classList.add('hide');
         p.textContent = practice.description;
-        //adds button
         let button = document.createElement("button");
         button.textContent = "Start";
         button.addEventListener('click', () => p.classList.toggle('hide'))
-        //create an element to hold facts
         //create title to quickies "Quickie Zone"
         dailyQuickies.append(h2, img, h5, p, button);
         document.getElementById("practicesList").appendChild(dailyQuickies);
@@ -31,9 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch("http://localhost:3000/practices")
         .then(res => res.json())
         .then(practices => {
-            //create a container for the practices ul
-            const practiceList = document.querySelector("ul#practicesList");
-            //loop through the practices (forEach meets 1 requirement)
             practices.forEach(practice => createContainerElements(practice));
         })
 
