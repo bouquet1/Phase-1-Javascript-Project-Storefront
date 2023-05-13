541f7f5d9df2d86943bcb50760676422394444fb
bouquet1 <bukuleta@gmail.com>
2023-05-12 21:54:47 -0700

    tries display the user input

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         p.classList.add('hide');
         p.textContent = practice.description;
         let button = document.createElement("button");
+        button.classList.add('quickie-button');
         button.textContent = `What is ${practice.name}`;
         button.addEventListener('click', () => p.classList.toggle('hide'));
         // button2.addEventListener('click', () => p.classList.toggle('hide'));
@@ -39,6 +40,8 @@ document.querySelector("#weeklyPlan").addEventListener("submit", (e) => {
     for(let i=0; i < numberOfDays; i++){
         //make d-down boxes === number of days
         const selectActivity = document.createElement("select");
+        const chooseOption = document.createElement("option")
+        chooseOption.textContent = "Choose Practice"
         const yoga = document.createElement("option")
         yoga.textContent = "Yoga"
         const meditation = document.createElement("option")
@@ -52,58 +55,89 @@ document.querySelector("#weeklyPlan").addEventListener("submit", (e) => {
         const pilates = document.createElement("option")
         pilates.textContent = "Pilates"
 
-        selectActivity.append(yoga, meditation, stretch, breathe, warmUp, pilates);
+        selectActivity.append(chooseOption, yoga, meditation, stretch, breathe, warmUp, pilates);
         displayDays.append(selectActivity)
     }
     displayDays.append(submitButton)
     displayDays.addEventListener('submit', handleDisplayDay)
 })
 
-function handleDisplayDay(e){
-    e.preventDefault()
-    const practiceContainer = document.querySelector('#practiceContainer')
-    console.log(practiceContainer)
-    if(displayDays[0]){ 
-        if(e.target[0] === displayDays[displayDays.length-1])return;
-        const dayOneCard = document.createElement("div")
-        const dayOnePractice = document.createElement('p')
-        const dayOne = document.createElement('h1')
-        dayOne.textContent = 'Day 1:'
-        dayOnePractice.textContent = e.target[0].value
-        dayOneCard.append(dayOne, dayOnePractice)
-        practiceContainer.append(dayOneCard)
+// function handleDisplayDay(e){
+//     e.preventDefault()
+//     const practiceContainer = document.querySelector('#practiceContainer')
+//     if(displayDays[0]){ 
+//         if(e.target[0] === displayDays[displayDays.length-1])return;
+//         const dayOneCard = document.createElement("div")
+//         const dayOnePractice = document.createElement('p')
+//         const dayOne = document.createElement('h1')
+//         dayOne.textContent = 'Day 1:'
+//         dayOnePractice.textContent = e.target[0].value
+//         dayOneCard.append(dayOne, dayOnePractice)
+//         practiceContainer.append(dayOneCard)
+//     }
+//     if(displayDays[1]){
+//         if(e.target[1] === displayDays[displayDays.length-1])return;
+//         console.log(e.target[1].value)
+//     }
+//     if(displayDays[2]){
+//         if(e.target[2] === displayDays[displayDays.length-1])return;
+//         console.log(e.target[2].value)
+//     }
+//     if(displayDays[3]){
+//         if(e.target[3] === displayDays[displayDays.length-1])return;
+//         console.log(e.target[3].value)
+//     }
+//     if(displayDays[4]){
+//         if(e.target[4] === displayDays[displayDays.length-1])return;
+//         console.log(e.target[4].value)
+//     }
+//     if(displayDays[5]){
+//         if(e.target[5] === displayDays[displayDays.length-1])return;
+//         console.log(e.target[5].value)
+//     }
+//     if(displayDays[6]){
+//         if(e.target[6] === displayDays[displayDays.length-1])return;
+//         console.log(e.target[6].value)
+//     }
+// }
+
+//QUESTION
+//I couldn't find a way to extract the value = user's choice from D-down menu such as 'Yoga'
+// function handleDisplayDay(e) {
+//     e.preventDefault();
+//     const practiceContainer = document.querySelector('#practiceContainer');
+//     const numberOfDays = document.getElementById("number-of-days").value;
+//     const selectActivity = document.createElement("select");
+//     const workPlan = {
+//         dayChoice: numberOfDays,
+//         practiceChoice: selectActivity.option, 
+//         //targetArea,
+//        //duration,
+//     }
+//     if(displayDays[0]) { 
+//         if(e.target [0] === displayDays[displayDays.length-1])return;
+//         const dayOneCard = document.createElement("div");
+//         dayOneCard.textContent = `Day ${workPlan.dayChoice}: r${workPlan.practiceChoice}`
+//         practiceContainer.append(dayOneCard)
+// }
+// }
+
+function handleDisplayDay(e) {
+        e.preventDefault();
+        const practiceContainer = document.querySelector('#practiceContainer');
+        if(displayDays[0]) { 
+            if(e.target [0] === displayDays[displayDays.length-1])return;
+            const dayOneCard = document.createElement("div");
+            dayOneCard.textContent = `Day ${numberOfDays}: r${workPlan.practiceChoice}`
+            practiceContainer.append(dayOneCard)
     }
-    if(displayDays[1]){
-        if(e.target[1] === displayDays[displayDays.length-1])return;
-        console.log(e.target[1].value)
     }
-    if(displayDays[2]){
-        if(e.target[2] === displayDays[displayDays.length-1])return;
-        console.log(e.target[2].value)
-    }
-    if(displayDays[3]){
-        if(e.target[3] === displayDays[displayDays.length-1])return;
-        console.log(e.target[3].value)
-    }
-    if(displayDays[4]){
-        if(e.target[4] === displayDays[displayDays.length-1])return;
-        console.log(e.target[4].value)
-    }
-    if(displayDays[5]){
-        if(e.target[5] === displayDays[displayDays.length-1])return;
-        console.log(e.target[5].value)
-    }
-    if(displayDays[6]){
-        if(e.target[6] === displayDays[displayDays.length-1])return;
-        console.log(e.target[6].value)
-    }
-}
 
 
-
-// 
 
 //manipulates affirmations box
-// const affirmations =document.getElementById("affirmations");
-// setTimeout(() => affirmations.show(), 3000);
-// document.getElementById("affirmationButton").addEventListener('click', (e) => affirmations.close())
+//QUESTION When I apply css on it, I can't close the modal box. I couldn't put it in the middle of the screen as a second layer. 
+
+// const quotes = document.getElementById("quotes");
+// setTimeout(() => quotes.show(), 3000);
+// document.getElementById("quotesButton").addEventListener("click", (e) => quotes.close())
